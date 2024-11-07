c46e7d2fa7428c312ce9457b50e10a6337a87607
Kitty <parkhabit@gmail.com>
2024-11-07 12:21:48 +1100

    styling for rhs sidebar on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HeaderBox from "../../components/HeaderBox";
 import TotalBalanceBox from "../../components/TotalBalanceBox";
+import RightSidebar from "../../components/RightSidebar";
 
 const Home = () => {
   const loggedIn = {
     firstName: "Kitty",
+    lastName: "Allen",
+    email: "k.allen91@gmail.com",
   };
   return (
     <section className="home">
@@ -23,7 +26,13 @@ const Home = () => {
             totalCurrentBalance={1250.25}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
