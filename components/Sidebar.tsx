537b460242e0d1aaca6252353c5e9dbee2108e24
Kitty <parkhabit@gmail.com>
2024-11-07 11:48:14 +1100

    add page navigation

---
+++ components/Sidebar.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { sidebarLinks } from "../constants";
+import { cn } from "../lib/utils";
+
+const Sidebar = ({ use }: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2 flex">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            alt="horizon logo"
+            className="size-[24px] max-xl:size-14"
+            height={34}
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            link.route === pathname || pathname.startsWith(`${link.route}/`);
+          return (
+            <Link
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+              href={link.route}
+              key={link.label}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={link.imgURL}
+                  alt={link.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
+      </nav>
+      FOOTER
+    </section>
+  );
+};
+
+export default Sidebar;
